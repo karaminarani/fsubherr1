f3727fdd0a85a4a503b4b023ef28feabbcde3b60
n/a <n/a>
2024-05-15 22:16:57 +0700

    chore: make it private

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 class Config:
     startmsg = (
@@ -16,18 +12,14 @@ class Config:
         'Join first, then press the Try Again button.'
     )
 
-    OWNER_ID = int(os.environ.get('OWNER_ID', ''))
+    API_ID = 2040
+    API_HASH = 'b18441a1ff607e10a989891a5462e627'
+    OWNER_ID = 487936750
+    MONGO_URL = 'mongodb://root:passwd@mongo'
+
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
     DATABASE_ID = int(os.environ.get('DATABASE_ID', ''))
-    MONGO_URL = os.environ.get('MONGO_URL', '')
 
-    API_ID = int(os.environ.get('API_ID', 2040))
-    API_HASH = (
-        os.environ.get(
-            'API_HASH',
-            'b18441a1ff607e10a989891a5462e627',
-        ),
-    )
     BOT_ID = BOT_TOKEN.split(':', 1)[0]
 
 
--- plugins/helpers.py
+++ plugins/helpers.py
@@ -2,6 +2,7 @@ import functools
 
 from pyrogram.errors import RPCError
 from pyrogram.helpers import ikb
+from pyrogram.types import CallbackQuery
 from pyrogram.types import Message
 
 from bot.client import Bot
@@ -27,13 +28,13 @@ class Helpers:
         self._client_.log.info('Admins Initialized')
         self.fsubcids = self.gvars('FSUB_IDS')
         self.protectc = self.gvars('PROTECT_CONTENT')[0]
-        self._client_.log.info(f'Protect: {self.protectc}')
+        self._client_.log.info(f'Protect {self.protectc}')
         self.startmsg = self.gvars('START_MESSAGE')[0]
         self._client_.log.info('Start Text Initialized')
         self.forcemsg = self.gvars('FORCE_MESSAGE')[0]
         self._client_.log.info('Force Text Initialized')
         self.generate = self.gvars('GEN_STATUS')[0]
-        self._client_.log.info(f'Generate: {self.generate}')
+        self._client_.log.info(f'Generate {self.generate}')
 
     def urlikb(self, text: str, url: str) -> ikb:
         return ikb([[(text, url, 'url')]])
@@ -70,9 +71,13 @@ class Helpers:
                     'title': title,
                     'ilink': ilink,
                 }
-                self._client_.log.info(f'FSubID-{i + 1} Initialized')
+                self._client_.log.info(
+                    f'FSubID-{i + 1} Initialized',
+                )
             except RPCError as e:
-                self._client_.log.warning(f'FSubID-{i + 1}: {e}')
+                self._client_.log.warning(
+                    f'FSubID-{i + 1}: {e}',
+                )
 
     async def usrikb(
         self,
@@ -103,7 +108,7 @@ class Helpers:
             )
         return ikb(layouts)
 
-    async def nojoin(self, user: int) -> list[int] or None:
+    async def nojoin(self, user: int) -> list[int] | None:
         joined = set()
         if not self.fsubcids or user in self.adminids:
             return None
@@ -183,9 +188,12 @@ helpers = Helpers(Bot)
 
 class Decorator:
     @staticmethod
-    def decorator(func) -> callable:
+    def filters(func: callable) -> callable:
         @functools.wraps(func)
-        async def wrapped(client, event):
+        async def wrapped(
+            _: Bot,
+            event: Message | CallbackQuery,
+        ):
             if hasattr(event, 'from_user'):
                 user = event.from_user.id
             elif (
@@ -197,11 +205,11 @@ class Decorator:
                 return
             if user not in helpers.adminids:
                 return
-            await func(client, event)
+            await func(_, event)
         return wrapped
 
-    def Admins(self, func) -> callable:
-        return self.decorator(func)
+    def admins(self, func: callable) -> callable:
+        return self.filters(func)
 
 
 decorator = Decorator()
